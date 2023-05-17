ba49e08574a21688730b7cf05314a4619ef34ffa
carolinadestepa <66126435+carolinadestepa@users.noreply.github.com>
2023-05-17 20:49:55 -0300

    Complete las entities

--- app/src/main/java/com/example/padle_match/entities/Club.kt
+++ app/src/main/java/com/example/padle_match/entities/Club.kt
@@ -3,10 +3,9 @@ package com.example.padle_match.entities
 data class Club(
     var cuit:String,
     var Nombre:String,
-    var provincia:String,
+    var partido:String,
+    var localidad:String,
     var domicilio:String,
     var email: String,
-    var telefonos:String,
-    var linkWebsite: String
-
+    var telefonos:String
 )
--- app/src/main/java/com/example/padle_match/entities/TournamentRepository.kt
+++ app/src/main/java/com/example/padle_match/entities/TournamentRepository.kt
@@ -7,11 +7,14 @@ class TournamentRepository {
     private val tournamentList: MutableList<Tournament> = mutableListOf()
 
     init{
+        /*
         tournamentList.add(Tournament("1","Club Chacarita - Torneo 2", "2022-02-03","22:00","C1",5,55.00,"Aplausos","url:Image" ) )
         tournamentList.add(Tournament("2","Club Chacarita - Torneo 2", "2022-02-03","22:00","C2",5,55.00,"Aplausos","url:Image" ) )
         tournamentList.add(Tournament("3","Club Chacarita - Torneo 2", "2022-02-03","22:00","C2",5,55.00,"Aplausos","url:Image" ) )
         tournamentList.add(Tournament("4","Club Chacarita - Torneo 2", "2022-02-03","22:00","C3",5,55.00,"Aplausos","url:Image" ) )
         tournamentList.add(Tournament("5","Club Chacarita - Torneo 2", "2022-02-03","22:00","C4",5,55.00,"Aplausos","url:Image" ) )
+
+         */
     }
 
     fun getTournaments () : MutableList<Tournament> {
