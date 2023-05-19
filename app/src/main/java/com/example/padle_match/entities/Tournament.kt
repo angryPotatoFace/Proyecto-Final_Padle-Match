38604ddf1a3d84950bfca6c9369ea70a23cca704
JuampiMasyk <102633645+JuampiMasyk@users.noreply.github.com>
2023-05-19 18:48:15 -0300

    Update Tournament.kt

--- app/src/main/java/com/example/padle_match/entities/Tournament.kt
+++ app/src/main/java/com/example/padle_match/entities/Tournament.kt
@@ -4,15 +4,16 @@ import android.media.Image
 import android.os.Parcelable
 import java.util.Date
 import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class Tournament(
     var idTorneo: String,
-    var club: Club,
+    var club: @RawValue Club,
     var titulo: String,
     var fecha: String,
     var hora: String,
-    var categoría:Categoria ,
+    var categoría:Categoria,
     var cupos: Number,
     var costoInscripción:Number,
     var premios: String,
