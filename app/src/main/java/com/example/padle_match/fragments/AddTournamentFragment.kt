32cd33bbd51abc16dbce13ef57b20c65a85b854c
LucianoRubin <rubin.luciano1@gmail.com>
2023-05-24 21:53:55 -0300

    Add tournament validations (ViewModel/XML)

--- app/src/main/java/com/example/padle_match/fragments/AddTournamentFragment.kt
+++ app/src/main/java/com/example/padle_match/fragments/AddTournamentFragment.kt
@@ -20,11 +20,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.padle_match.R
 import com.example.padle_match.adapter.TournamentAdapter
 import com.example.padle_match.entities.TournamentRepository
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import java.util.*
 
 class AddTournamentFragment: Fragment()  {
@@ -34,8 +38,20 @@ class AddTournamentFragment: Fragment()  {
     private lateinit var adapter: TournamentAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var btnAddTournament: Button
+    private lateinit var editName: EditText
+    private lateinit var editClub: AutoCompleteTextView
+    private lateinit var editFecha: EditText
+    private lateinit var editHorario: EditText
+    private lateinit var editCategorias: AutoCompleteTextView
+    private lateinit var editMateriales: AutoCompleteTextView
+    private lateinit var editCupo: EditText
+    private lateinit var editCosto: EditText
+    private lateinit var editPremios: EditText
+    private lateinit var editImagen : EditText
+    private lateinit var addBtn : Button
 
     val db = Firebase.firestore
+
     companion object {
         fun newInstance() = AddTournamentFragment()
     }
@@ -46,9 +62,18 @@ class AddTournamentFragment: Fragment()  {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         v = inflater.inflate(R.layout.fragment_add_tournament, container, false)
-
+        editName = v.findViewById(R.id.editTextAddTournament_name)
+        editClub = v.findViewById(R.id.editTextAddTournament_club)
+        editFecha = v.findViewById(R.id.editTextAddTournament_fecha)
+        editHorario = v.findViewById(R.id.editTextAddTournament_horario)
+        editCategorias = v.findViewById(R.id.editTextAddTournament_categorias)
+        editMateriales = v.findViewById(R.id.editTextAddTournament_materiales)
+        editCupo = v.findViewById(R.id.editTextAddTournament_cupo)
+        editCosto = v.findViewById(R.id.editTextAddTournament_costo)
+        editPremios = v.findViewById(R.id.editTextAddTournament_premios)
+        editImagen = v.findViewById(R.id.editTextAddTournament_imagen)
+        addBtn = v.findViewById(R.id.addTournamentButton)
         return v
     }
 
@@ -63,20 +88,42 @@ class AddTournamentFragment: Fragment()  {
         super.onStart()
 
         // ============== CREO OBJETO DATE PICKER =================
+        // Permite solo seleccionar una fecha a partir del dia actual
+        val today = MaterialDatePicker.todayInUtcMilliseconds()
         val datePicker =
             MaterialDatePicker.Builder.datePicker()
                 .setTitleText("Seleccione una fecha")
-                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setSelection(today)
+                .setCalendarConstraints(
+                    CalendarConstraints.Builder()
+                        .setValidator(DateValidatorPointForward.from(today))
+                        .build()
+                )
                 .build()
 
-        var fecha: EditText = v.findViewById(R.id.fechaEditText)
-
         // ============== CUANDO CLIKEAN EL CAMPO FECHA SE ACTIVA EL DATE PICKER =================
-        fecha.setOnClickListener{
+        editFecha.setOnClickListener{
             datePicker.show(requireActivity().supportFragmentManager, "tag" )
             datePicker.addOnPositiveButtonClickListener { selection ->
                 val dateString = DateFormat.format("dd/MM/yyyy", Date(selection)).toString()
-                fecha.setText(dateString)
+                editFecha.setText(dateString)
+            }
+        }
+
+        // ============== CREO OBJETO TIME PICKER =================
+        val timePicker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setTitleText("Seleccione una hora")
+                .build()
+
+        // ============== CUANDO CLIKEAN EL CAMPO HORA SE ACTIVA EL TIME PICKER =================
+        editHorario.setOnClickListener {
+            timePicker.show(requireActivity().supportFragmentManager, "tag")
+            timePicker.addOnPositiveButtonClickListener {
+                val hour = timePicker.hour
+                val minute = timePicker.minute
+                editHorario.setText(String.format("%02d:%02d", hour, minute))
             }
         }
 
@@ -86,7 +133,7 @@ class AddTournamentFragment: Fragment()  {
             .addOnSuccessListener { clubs ->
                 val data: List<String> = clubs.map { it -> it.data["nombre"] } as List<String>
                 val d: Array<String> = data.toTypedArray()
-                var clubList:AutoCompleteTextView = v.findViewById(R.id.input_list_clubs)
+                var clubList:AutoCompleteTextView = editClub
                 (clubList as? MaterialAutoCompleteTextView)?.setSimpleItems(d)
             }
             .addOnFailureListener { exception ->
@@ -101,7 +148,7 @@ class AddTournamentFragment: Fragment()  {
                 val data: List<String> = categorias.map { it -> it.data["nombreCategoria"] }[0] as List<String>
                 val categorias: Array<String> = data.toTypedArray()
                 Log.w("CATEGORIAS", categorias.toString())
-                var categoriaList:AutoCompleteTextView = v.findViewById(R.id.autoCompleteTextView2)
+                var categoriaList:AutoCompleteTextView = editCategorias
                 (categoriaList as? MaterialAutoCompleteTextView)?.setSimpleItems(categorias as Array<String>)
             }
             .addOnFailureListener { exception ->
@@ -115,7 +162,7 @@ class AddTournamentFragment: Fragment()  {
             .addOnSuccessListener { materiales ->
                 val data: List<String> = materiales.map { it -> it.data["materiales"] }[0] as List<String>
                 val mat: Array<String> = data.toTypedArray()
-                var mateList:AutoCompleteTextView = v.findViewById(R.id.listMateriales)
+                var mateList:AutoCompleteTextView = editMateriales
                 (mateList as? MaterialAutoCompleteTextView)?.setSimpleItems(mat as Array<String>)
             }
             .addOnFailureListener { exception ->
@@ -124,18 +171,18 @@ class AddTournamentFragment: Fragment()  {
 
 
         // ============== SETEO EL CAMPO DE SELECCION DE IMAGEN =================
-        val imagen : EditText = v.findViewById(R.id.ImagenEditText)
-        imagen.setOnClickListener{
+        editImagen.setOnClickListener{
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, 100)
         }
+        
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         // =========== SI SE CARGO LA IMAGEN CORRECTAMENTE SE MUESTRA =================
-        val imagen : ImageView = v.findViewById(R.id.imagen)
+        val imagen : ImageView = v.findViewById(R.id.AddTournamentimagen)
         if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
             val imageUri = data.data
             imagen.setImageURI(imageUri)
--- app/src/main/java/com/example/padle_match/fragments/AddTournamentViewModel.kt
+++ app/src/main/java/com/example/padle_match/fragments/AddTournamentViewModel.kt
@@ -1,7 +1,46 @@
 package com.example.padle_match.fragments
 
+
+import android.icu.text.SimpleDateFormat
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import androidx.lifecycle.ViewModel
+import java.util.*
+
 
 class AddTournamentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    companion object{
+        private val MSG_CAMPO_REQUERIDO = "Campo requerido"
+    }
+    fun validateFields(
+        editName: EditText,
+        editClub: AutoCompleteTextView,
+        editFecha: EditText,
+        editHorario: EditText,
+        editCategorias: AutoCompleteTextView,
+        editMateriales: AutoCompleteTextView,
+        editCupo: EditText,
+        editCosto: EditText,
+        editPremios: EditText,
+        editImagen: EditText
+    ) : Boolean {
+        return notEmpty(editName) && notEmpty(editClub) && notEmpty(editFecha) && notEmpty(editHorario) && notEmpty(editCategorias)
+                && notEmpty(editMateriales) && notEmpty(editCupo) && notEmpty(editCosto) && notEmpty(editPremios) && notEmpty(editImagen)
+    }
+
+    private fun notEmpty(editText: EditText) : Boolean {
+        var isValid : Boolean = true
+        val value = editText.text.toString().trim()
+        if(value.isNullOrEmpty()){
+            editText.error = MSG_CAMPO_REQUERIDO
+            isValid = false
+        }
+        return isValid
+    }
+    private fun notEmpty(autoCompleteTextView: AutoCompleteTextView): Boolean {
+        val editText: EditText = autoCompleteTextView
+        return notEmpty(editText)
+    }
+
 }
